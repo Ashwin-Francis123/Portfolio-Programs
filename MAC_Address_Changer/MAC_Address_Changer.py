32780e835e551df6723cc986772b96791e313b9f
Ashwin-Francis123 <87746421+Ashwin-Francis123@users.noreply.github.com>
2021-12-13 14:46:09 +0800

    Update MAC_Address_Changer.py

--- MAC_Address_Changer/MAC_Address_Changer.py
+++ MAC_Address_Changer/MAC_Address_Changer.py
@@ -1,8 +1,3 @@
-# This program allows you to change your MAC Address. It lets you specify an interface with the -i or --interface switches
-# For Example: kali@kali:~$ python3 Random_MAC_Address.py -i eth0
-# The program will then provide and change your MAC Address to a random one.
- 
- 
 # Modules
 import subprocess
 import random
